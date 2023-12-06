fc9d1145e3ee948f84c581b7be9b1f4e4dc12ed7
pradeepks780 <pradeepks780@gmail.com>
2023-12-06 15:55:36 +0530

    display 2 and 3 removed

--- src/com/pk/git/PKGit.java
+++ src/com/pk/git/PKGit.java
@@ -5,8 +5,7 @@ public class PKGit {
 	public static void main(String[] args) {
 		
 		System.out.println("display 1");
-		System.out.println("display 2");
-		System.out.println("display 3");
+
 
 	}
 }
